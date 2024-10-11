d10ec557cd710327690a6b10abd25c2d2184701d
14tp00 <118660337+14tp000@users.noreply.github.com>
2024-10-11 20:18:11 +0200

    Revert "Shaderki"
    This reverts commit e1d5055472db878ed2488f739f36356276411b38.

--- LightOfTheGoldenFeather/Assets/Scripts/PlayerMovement.cs
+++ LightOfTheGoldenFeather/Assets/Scripts/PlayerMovement.cs
@@ -14,9 +14,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float lightDownSpeed = 0.003f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask wallLayer;
-
-    [SerializeField] private Material mat;
-
     private Rigidbody2D body;
     private DoorRespawn respawn;
     private float auraScale = 1;
@@ -54,7 +51,6 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
-<<<<<<< Updated upstream:LightOfTheGoldenFeather/Assets/Scripts/PlayerMovement.cs
         if (lockPlayer)
         {
             return;
@@ -65,13 +61,6 @@ public class PlayerMovement : MonoBehaviour
             StartCoroutine(GameOver());
         }
         playerLight.gameObject.transform.localScale = new Vector3(auraScale*6, auraScale*4, 0);
-=======
-        float x = Camera.main.WorldToScreenPoint(transform.position).x/Screen.height;
-        float y = Camera.main.WorldToScreenPoint(transform.position).y/Screen.height;
-        mat.SetVector("_position",new Vector2(x,y)); // TODO: Ogarn¹c tego ¿ycie, to powinno byæ w osobnym skrypcie który ogarnia wszystkie na raz
-
-
->>>>>>> Stashed changes:LightOfTheGoldenFeather/Assets/Scripts/Player.cs
         horizontalInput = Input.GetAxis("Horizontal");
         //Flip player when moving left-right
         if (horizontalInput > 0.01f)
