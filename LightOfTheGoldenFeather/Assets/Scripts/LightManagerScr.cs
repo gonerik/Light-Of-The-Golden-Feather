935e113aff5ebbe9ae489ddac6b6541f3909e1fb
IgorSienkiewicz <147891614+SkilluDev@users.noreply.github.com>
2024-10-29 16:40:00 +0100

    Added welcome message

--- LightOfTheGoldenFeather/Assets/Scripts/LightManagerScr.cs
+++ LightOfTheGoldenFeather/Assets/Scripts/LightManagerScr.cs
@@ -13,7 +13,7 @@ public class LightManagerScr : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hello");
     }
 
     // Update is called once per frame
